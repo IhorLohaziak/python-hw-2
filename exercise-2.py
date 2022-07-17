fd142d75bcd8ef9c0beb955cb82638de5dc8c29c
IhorLohaziak <lordlir@ukr.net>
2022-07-17 09:53:03 +0300

    second commit

--- exercise-2.py
+++ exercise-2.py
@@ -3,11 +3,17 @@ def transformStr():
     a = len(string)
     b = string[0]
     if a > 5:
-      print(string[0:5] + '...') 
-      if b == 'U' or b == 'u':
-        print(string.upper())
-      elif b == 'L' or b == 'l':
-        print(string.lower())
-    else:
-        print(string)
+        if b == 'U' or b == 'u':
+            print(string[0:5].upper() + '...')
+        elif b == 'L' or b == 'l':
+            print(string[0:5].lower() + '...')
+        else:
+            print(string[0:5] + '...')
+    elif a < 5:
+        if b == 'U' or b == 'u':
+            print(string.upper())
+        elif b == 'L' or b == 'l':
+            print(string.lower())
+        else:
+            print(string)
 transformStr()
