db25bd80a229d61688c1b741ab097998145ef927
IhorLohaziak <lordlir@ukr.net>
2022-07-18 17:48:08 +0300

    second commit

--- exercise-additional-1.2.py
+++ exercise-additional-1.2.py
@@ -5,7 +5,15 @@ def sumNum(num):
     r = str(c)
     z = len(r)
     if z == 2:
-        print(r[1])
+        a1 = int(r)
+        if a1 < 10:
+            print(r[1])
+        elif a1 >= 10:
+            f = str(a1)
+            f1 = int(f[0])
+            f2 = int(f[1])
+            f3 = f1 + f2
+            print(f3)
     elif z == 1:
         print(r[0])
 sumNum(num)
